4019703d30e22d63d816e5c9e6536e5b64291974
Mia Zheng <zhengmiao1@gmail.com>
2017-03-18 22:15:27 -0700

    Q5

---
+++ FiveTriangle.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class FiveTriangle {
+	public List<Integer> getRow(int rowIndex) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		 
+		if (rowIndex < 0)
+			return al;
+	 
+		al.add(1);
+		for (int i = 1; i <= rowIndex; i++) {
+			for (int j = al.size() - 2; j >= 0; j--) {
+				al.set(j + 1, al.get(j) + al.get(j + 1));
+			}
+			al.add(1);
+		}
+		return al;
+	}
+
+}
